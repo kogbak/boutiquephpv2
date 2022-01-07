ff6ca5398740f9c6c6b7d673ca68b76b45d02e46
kogbak <poirault.benjamin@hotmail.fr>
2022-01-07 15:41:21 +0100

    reparation git loooooool

--- modifier_mes_informations.php
+++ modifier_mes_informations.php
@@ -25,10 +25,29 @@ session_start();
 
 if(isset($_POST["informations"])){
 
-    
-}
+}?>
 
-        ?>
+<h3 class="text-center">Modifier mes informations</h3>
+
+
+<form class="w-50 mx-auto" action="index.php" method="post">
+  <div class="mb-3 mt-5">
+    <label for="nom" class="form-label">Nom: </label>
+    <input required type="text" class="form-control" name="nom" value=" <?php $_SESSION["nom"] ?> "> 
+  </div>
+  <div class="mb-3">
+    <label for="prenom" class="form-label">Prénom: </label>
+    <input required type="text" class="form-control" name="prenom">
+  </div>
+  <div class="mb-3 ">
+    <label for="Email" class="form-label">Adresse email: </label>
+    <input required type="email" class="form-control" name="email" aria-describedby="emailHelp">
+  </div>
+
+  
+  <button type="submit" class="btn btn-primary mb-5">Valider</button>
+  <input type="hidden" name="bouton_valider" value="true">
+</form>
 
 
     </div>
